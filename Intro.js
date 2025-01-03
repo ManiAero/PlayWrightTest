76be20b3c40c649eed998c65ed54a90079d828f2
ManiAero <maniaero577@gmail.com>
2025-01-03 16:19:23 +0530

    Change

--- Intro.js
+++ Intro.js
@@ -13,9 +13,47 @@
 //     page.goto("https://www.facebook.com/")
 // })();
 
-const{chromium} = require("playwright");
-(async () => {
+// const{chromium} = require("playwright");
+// (async () => {
+//     const browser = await chromium.launch({headless:false});
+//     const page = await browser.newPage();
+//     page.goto("http://www.facebook.com")
+//     page
+// })();
+
+// const {firefox}= require('playwright');
+// (async()=>{
+//     const browser= await firefox.launch({headless:false});
+//     const page= await browser.newPage();
+//     page.goto("http://www.facebook.com")
+// }
+
+// )();
+
+// const {chromium }= require('playwright');
+// (async()=>{
+//     const browser= await chromium.launch({headless:false});
+//     const page = await browser.newPage();
+//     page.goto("http://www.amazon.com")
+// }
+
+// )();
+
+const {firefox}= require('playwright') ;
+(async()=>{
+const browser = await firefox.launch({headless:false});
+const page= await browser.newPage();
+page.goto("https://www.amazon.in/")
+
+}
+)();
+
+const {chromium} = require ('playwright');
+(async()=> {
+
     const browser = await chromium.launch({headless:false});
     const page = await browser.newPage();
-    page.goto("http://www.facebook.com")
-})();
+    page.goto("https://www.flipkart.com/")
+}
+
+)();
